fa4a76ad354e3579e6ac2a57d7484e11ea628b88
Thomas Reyes <thomasellison.r@gmail.com>
2017-11-09 10:31:56 -0800

    updated databases for identity lab

---
+++ Lab28Tom/Migrations/ApplicationDb/20171020221621_addedproperties.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Lab28Tom.Migrations.ApplicationDb
+{
+    public partial class addedproperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PowerLevel",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PowerLevel",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ Lab28Tom/Models/LFG.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab28Tom.Models
+{
+    public class LFG
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Gamertag/PSN ID")]
+        public string Gamertag { get; set; }
+
+        [Display(Name = "Class")]
+        public string DestinyClass { get; set; }
+
+        [Display(Name = "Notes")]
+        public string Request { get; set; }
+    }
+}
